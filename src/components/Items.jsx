486751e2b4a814e8a7b6698547f51af9a1890bca
Shamel <shamel.rai@gmail.com>
2024-07-04 18:24:29 +0545

    Ready

--- src/components/Items.jsx
+++ src/components/Items.jsx
@@ -1,10 +1,11 @@
-import styles from './items.module.css'; 
+import styles from './items.module.css';
 export default function Items({ items }) {
     return (
         <div>
             <div className={styles.itemContainer}>
                 <div className={styles.imgContainer}>
-                    <img src={`https://spoonacular.com/cdn/ingredients_100x100/${items.image}`} alt="" />
+                    <img className={styles.img}
+                        src={`https://spoonacular.com/cdn/ingredients_100x100/${items.image}`} alt="" />
                 </div>
                 <div className={styles.nameContainer}>
                     <div className={styles.name}>
