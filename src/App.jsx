29c0d501a8bdab2f55da5fc8355f429ab9eff193
Shamel <shamel.rai@gmail.com>
2024-07-04 15:34:26 +0545

    Adding style to the application

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import { useState } from "react";
 import Search from "./components/Search";
+import FoodList from "./components/FoodList";
 
 function App() {
   const [foodData, setFoodDate] = useState([]);
   return (
     <div className="App">
       <Search foodData={foodData} setFoodDate={setFoodDate} />
-      {foodData.map((food) => <h1>{food.title}</h1>)}
+      <FoodList foodData={foodData} />
+
     </div>
   )
 
---
+++ src/components/FoodList.jsx
@@ -0,0 +1,13 @@
+import FoodItem from "./FoodItem";
+
+export default function FoodList({ foodData }) {
+
+    return (
+        <div>
+            {foodData.map((food) => (
+                <FoodItem food={food} key={food.id} />
+            )
+            )}
+        </div>
+    )
+}
